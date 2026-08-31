b5c4c82913e738aef67e4143f932e899fc9bafc0
Lucifermeooow <119759050+Lucifermeooow@users.noreply.github.com>
2026-08-31 04:11:16 +0300

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,26 +1,46 @@
 pluginManagement {
-    val flutterSdkPath = run {
-        val properties = java.util.Properties()
-        file("local.properties").inputStream().use { properties.load(it) }
-        val path = properties.getProperty("flutter.sdk")
-        require(path != null) { "flutter.sdk not set in local.properties" }
-        path
-    }
+    val flutterSdkPath =
+        run {
+            val properties = java.util.Properties()
 
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+            file("local.properties")
+                .inputStream()
+                .use { properties.load(it) }
+
+            properties.getProperty("flutter.sdk")
+                ?: error("flutter.sdk not set in local.properties")
+        }
+
+    includeBuild(
+        "$flutterSdkPath/packages/flutter_tools/gradle"
+    )
 
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven { url = java.net.URI("https://jitpack.io") }
     }
 }
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.8.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+
+    id("com.android.application") version "9.0.1" apply false
+
+    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
 }
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+}
+
+rootProject.name = "stream22"
 
 include(":app")
