8a991e0503da208252f8724d63bea32287c06992
Lucifermeooow <119759050+Lucifermeooow@users.noreply.github.com>
2026-08-31 04:44:55 +0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,36 +1,10 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:stream_22/main.dart';
+import 'package:streamgit101/main.dart';
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
+  testWidgets('StreamGit101 renders', (WidgetTester tester) async {
+    await tester.pumpWidget(const StreamV21App());
 
-  setUpAll(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-        .setMockMethodCallHandler(
-      const MethodChannel('flutter.baseflow.com/permissions/methods'),
-      (MethodCall methodCall) async => 1,
-    );
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-        .setMockMethodCallHandler(
-      const MethodChannel('plugins.flutter.io/camera'),
-      (MethodCall methodCall) async => [],
-    );
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-        .setMockMethodCallHandler(
-      const MethodChannel('rtmp_streaming'),
-      (MethodCall methodCall) async => null,
-    );
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-        .setMockMethodCallHandler(
-      const MethodChannel('wakelock_plus'),
-      (MethodCall methodCall) async => null,
-    );
-  });
-
-  testWidgets('Stream 22 basic smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const Stream22App());
-    await tester.pump();
-    expect(find.byType(Stream22App), findsOneWidget);
+    expect(find.byType(StreamV21App), findsOneWidget);
   });
 }
