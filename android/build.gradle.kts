a358b1807bd8a008832495a696f4df9596fac64d
Lucifermeooow <119759050+Lucifermeooow@users.noreply.github.com>
2026-08-31 04:55:02 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,15 +2,10 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+
         maven {
             url = java.net.URI("https://jitpack.io")
         }
-    }
-
-    tasks.matching {
-        it.name.contains("AarMetadata")
-    }.configureEach {
-        enabled = false
     }
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
