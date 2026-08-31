f1d06a14bbf4c6019537ad661e142837850c669a
Lucifermeooow <119759050+Lucifermeooow@users.noreply.github.com>
2026-08-31 04:46:52 +0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,9 +2,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:streamgit101/main.dart';
 
 void main() {
-  testWidgets('StreamGit101 renders', (WidgetTester tester) async {
-    await tester.pumpWidget(const StreamV21App());
+  testWidgets('Stream 22 renders', (WidgetTester tester) async {
+    await tester.pumpWidget(const Stream22App());
 
-    expect(find.byType(StreamV21App), findsOneWidget);
+    expect(find.byType(Stream22App), findsOneWidget);
   });
 }
